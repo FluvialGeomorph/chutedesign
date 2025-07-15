433fff1df539fde2e5be6ef84499d8b00f9a7f31
Michael <mike.p.doc.71@gmail.com>
2025-07-15 17:24:12 -0500

    restructured layout and started adding variables

--- R/app_ui.R
+++ R/app_ui.R
@@ -11,10 +11,31 @@ app_ui <- function(request) {
 
     page_navbar(
       title = "Chute Design",
-
-      layout_sidebar(
-        sidebar = sidebar(
-          "Sidebar fields",
+      id = "main",
+      theme = bs_theme(bootswatch = "solar", version = 5),
+      nav_panel(
+        title = "Design a Chute",
+        layout_sidebar(
+          sidebar = sidebar(
+            width = 400,
+            accordion(
+              accordion_panel(
+                "Chute Parameters", icon = bsicons::bs_icon("rulers"),
+                numericInput("width", "Width of Chute (m)", 10),
+                numericInput("length", "Length of Chute (m)", 2000),
+                numericInput("slope", "Slope of Chute (m/m)", 0.01),
+                numericInput("side_slope", "Side Slope (h:v)", 2.5),
+                numericInput("total_discharge", "Total Discharge (m^3/s)", 2000),
+                numericInput("particle_size", "Particle Size (m)", 2.6)
+              ),
+              accordion_panel(
+                "Constants", icon = bsicons::bs_icon("sliders"),
+                numericInput("gravity", "Gravity (N/m^3)", 9.787),
+                numericInput("water_density", "Water Density (kg/m^3)", 998),
+                numericInput("stone_density", "Stone Density (kg/m^3)", 2650)
+              )
+            ), # End accordion
+          ), # End sidebar
           navset_card_tab(
             nav_panel(
               title = "Plot 1",
@@ -25,9 +46,8 @@ app_ui <- function(request) {
               "Plot 2 content"
             )
           ) # End navset_card_tab
-        ) # End sidebar
-      ) # End layout_sidebar
+        ) # End layout_sidebar
+      ) # End page_navbar
     ) # End page_navbar
   )
 }
-
