1ce2919e43c46716f38f5b3eace1636a00f3f919
Michael <mike.p.doc.71@gmail.com>
2025-07-22 17:03:18 -0500

    refactored plot_stone_size_method to handle slope

--- R/plot_stone_size_method.R
+++ R/plot_stone_size_method.R
@@ -2,6 +2,8 @@
 #' @description Plot estimated stone size according to several different
 #'              methods.
 #' @param channel_dims data frame; A channel dimensions data frame.
+#' @param x_axis       character; The variable to use for the x-axis. One of
+#'                     "width", "slope"
 #' @returns a ggplot2 object
 #' @export
 #' @importFrom tidyr pivot_longer starts_with
@@ -9,7 +11,7 @@
 #' @importFrom dplyr mutate
 #' @importFrom ggplot2 ggplot aes geom_line scale_color_brewer labs
 #'             theme_light
-plot_stone_size_method <- function(channel_dims) {
+plot_stone_size_method <- function(channel_dims, x_axis) {
   # Convert from wide to long for plotting
   dims_long <- channel_dims %>%
     pivot_longer(
@@ -20,11 +22,25 @@ plot_stone_size_method <- function(channel_dims) {
     mutate(method = str_remove(.data$method, "stone_size_")) %>%
     mutate(method = factor(.data$method))
 
-  ggplot(dims_long,
-         aes(x = width, y = stone_size, color = method)) +
-    geom_line(size = 1.8) +
-    scale_color_brewer(palette = "Dark2") +
-    labs(title = "Stone Size by Methods",
-         x = "Width (m)", y = "Stone Size (m)") +
-    theme_light()
+  if(x_axis == "width") {
+    stone_plot <- ggplot(dims_long,
+                         aes(x = width, y = stone_size, color = method)) +
+      geom_line(size = 1.8) +
+      scale_color_brewer(palette = "Dark2") +
+      labs(title = "Stone Size by Methods",
+           x = "Width (m)", y = "Stone Size (m)") +
+      theme_light()
+    return(stone_plot)
+  }
+
+  if(x_axis == "slope") {
+    stone_plot <- ggplot(dims_long,
+                         aes(x = slope, y = stone_size, color = method)) +
+      geom_line(size = 1.8) +
+      scale_color_brewer(palette = "Dark2") +
+      labs(title = "Stone Size by Methods",
+           x = "Slope", y = "Stone Size (m)") +
+      theme_light()
+    return(stone_plot)
+  }
 }
