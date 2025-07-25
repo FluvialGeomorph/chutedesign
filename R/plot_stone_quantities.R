e39a779d15aac8786be0cce00ce7415e90c92fde
Michael <mike.p.doc.71@gmail.com>
2025-07-25 11:26:57 -0500

    added plot_stone_quantities functions

---
+++ R/plot_stone_quantities.R
@@ -0,0 +1,69 @@
+#' @title Plot Channel Stone Quantities
+#' @description Create a plot of estimated stone quantities from a channel
+#'              dimensions data frame.
+#' @param channel_dims data frame; A channel dimensions data frame.
+#' @param x_axis       character; The variable to use for the x-axis. One of
+#'                     "width", "slope", "particle_size"
+#' @returns a ggplot object
+#' @export
+#' @importFrom dplyr %>% mutate
+#' @importFrom tidyr pivot_longer
+#' @importFrom rlang sym
+#' @importFrom ggplot2 ggplot aes geom_line facet_wrap labs theme_light theme
+#'             element_text element_blank
+#' @importFrom stringr str_to_title
+plot_stone_quantities <- function(channel_dims, x_axis) {
+  # Validate x_axis
+  valid_x_axes <- c("width", "slope", "particle_size")
+  if (!(x_axis %in% valid_x_axes)) {
+    stop("x_axis must be one of: ", paste(valid_x_axes, collapse = ", "))
+  }
+
+  # Human-readable labels
+  param_labels <- c(
+    adopted_stone_diameter = "Median Stone Diameter (m)",
+    mattress_thickness = "Mattress Thickness (m)",
+    adopted_stone_weight_ton = "Median Stone Weight (US ton)",
+    stone_vol_us_ton = "Stone Volume (US ton)",
+    number_stones = "Number of Stones (count)"
+    )
+
+  # Select and pivot to long format
+  stone_vars <- channel_dims %>%
+    select({{x_axis}},
+           adopted_stone_diameter,
+           mattress_thickness,
+           adopted_stone_weight_ton,
+           stone_vol_us_ton,
+           number_stones) %>%
+    pivot_longer(
+      cols = -{{x_axis}},
+      names_to = "parameter",
+      values_to = "value") %>%
+    mutate(parameter = factor(.data$parameter,
+                              levels = names(param_labels),
+                              labels = param_labels))
+
+  # Determine x_axis labels
+  x_axis_label <- switch(x_axis,
+                         width         = "Width (m)",
+                         slope         = "Slope",
+                         particle_size = "Particle Size (m)")
+
+  # Faceted plot
+  flow_plot <- ggplot(stone_vars, aes(x = !!sym(x_axis), y = value)) +
+    geom_line(color = "#990006FF", linewidth = 1) +
+    facet_wrap(~parameter, scales = "free_y") +
+    labs(
+      title = "Stone Quantities",
+      x = x_axis_label,
+      y = NULL
+    ) +
+    theme_light(base_size = 13) +
+    theme(
+      strip.text = element_text(face = "bold"),
+      panel.grid.minor = element_blank()
+    )
+  #flow_plot
+  return(flow_plot)
+}
---
+++ tests/testthat/test_plot_stone_quantities.R
@@ -0,0 +1,101 @@
+test_that("check by_width_df", {
+  scenario <- create_scenario (
+    width               = 60,
+    width_start         = 40,
+    width_end           = 200,
+    width_by            = 20,
+    length              = 2000,
+    length_start        = 1000,
+    length_end          = 3000,
+    length_by           = 500,
+    slope               = 0.01,
+    slope_start         = 0,
+    slope_end           = 1,
+    slope_by            = 0.05,
+    particle_size       = 2.6,
+    particle_size_start = 1,
+    particle_size_end   = 3,
+    particle_size_by    = 0.1,
+    side_slope          = 2.5,
+    total_discharge     = 2000,
+    stone_density       = 2650,
+    contingency         = 1.3,
+    porosity            = 0.3,
+    water_density       = 998,
+    gravity             = 9.787
+  )
+  width_df <- by_width_df(scenario)
+  width_dims <- channel_dimensions(width_df)
+  x_axis <- "width"
+  plot1 <- plot_stone_quantities(width_dims, x_axis = x_axis)
+  plot1
+  expect_s3_class(plot1, "ggplot")
+})
+
+test_that("check by_slope_df", {
+  scenario <- create_scenario (
+    width               = 60,
+    width_start         = 40,
+    width_end           = 200,
+    width_by            = 20,
+    length              = 2000,
+    length_start        = 1000,
+    length_end          = 3000,
+    length_by           = 500,
+    slope               = 0.01,
+    slope_start         = 0,
+    slope_end           = 1,
+    slope_by            = 0.05,
+    particle_size       = 2.6,
+    particle_size_start = 1,
+    particle_size_end   = 3,
+    particle_size_by    = 0.1,
+    side_slope          = 2.5,
+    total_discharge     = 2000,
+    stone_density       = 2650,
+    contingency         = 1.3,
+    porosity            = 0.3,
+    water_density       = 998,
+    gravity             = 9.787
+  )
+  slope_df <- by_slope_df(scenario)
+  slope_dims <- channel_dimensions(slope_df)
+  x_axis = "slope"
+  plot2 <- plot_stone_quantities(slope_dims, x_axis = x_axis)
+  plot2
+  expect_s3_class(plot2, "ggplot")
+})
+
+test_that("check by_particle_size_df", {
+  scenario <- create_scenario (
+    width               = 60,
+    width_start         = 40,
+    width_end           = 200,
+    width_by            = 20,
+    length              = 2000,
+    length_start        = 1000,
+    length_end          = 3000,
+    length_by           = 500,
+    slope               = 0.01,
+    slope_start         = 0,
+    slope_end           = 1,
+    slope_by            = 0.05,
+    particle_size       = 2.6,
+    particle_size_start = 1,
+    particle_size_end   = 3,
+    particle_size_by    = 0.1,
+    side_slope          = 2.5,
+    total_discharge     = 2000,
+    stone_density       = 2650,
+    contingency         = 1.3,
+    porosity            = 0.3,
+    water_density       = 998,
+    gravity             = 9.787
+  )
+  particle_size_df <- by_particle_size_df(scenario)
+  particle_size_dims <- channel_dimensions(particle_size_df)
+  x_axis = "particle_size"
+  plot3 <- plot_stone_quantities(particle_size_dims, x_axis = x_axis)
+  plot3
+  expect_s3_class(plot3, "ggplot")
+})
